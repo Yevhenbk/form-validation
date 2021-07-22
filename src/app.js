6f224e3ae9d9b6ab67b3bc7be58b4e561383e8d0
Yevhenbk <balagutrak.jenia@gmail.com>
2021-07-22 15:20:41 +0000

    un mensaje

--- src/app.js
+++ src/app.js
@@ -5,7 +5,133 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const NAME = document.querySelector("#First");
+const SURNAME = document.querySelector("#Last");
+const CARD = document.querySelector("#cc-number");
+const CVC = document.querySelector("#cc-cvc");
+const AMOUNT = document.querySelector("#cc-amount");
+const CITY = document.querySelector("#inputCity");
+const STATE = document.querySelector("#inputState");
+const ZIP = document.querySelector("#inputZip");
+const CARDS = document.querySelector("#input-group-prepend");
+const SEND = document.querySelector("#Send");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  SEND.addEventListener("click", function(event) {
+    event.preventDefault();
+  });
+  NAMEVALIDATION();
+  LASTNAMEVALIDATION();
+  CARDVALIDATION();
+  CVCVALIDATION();
+  AMOUNTVALIDATION();
+  CITYVALIDATION();
+
+  /*checkcvc();
+  checkname();
+  checklastname();
+  checkcard();
+  checkamount();
+  checkstate();
+  checkcity();
+  checkzip();
+  isValidCreditCard();*/
 };
+
+/*const checkcvc = () => {
+  CVC.addEventListener("focusout", function(event) {
+    console.log("submit");
+    event.target.style.background = "pink";
+  });
+};
+const checkname = () => {
+  NAME.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checklastname = () => {
+  SURNAME.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checkcard = () => {
+  CARD.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checkamount = () => {
+  AMOUNT.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checkstate = () => {
+  STATE.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checkcity = () => {
+  CITY.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};
+const checkzip = () => {
+  ZIP.addEventListener("focusout", function(event) {
+    event.target.style.background = "pink";
+  });
+};*/
+const CHECKNUMBERS = myNumber => {
+  return Number(myNumber) % 1 == 0;
+};
+const CHECKSTRING = string => {
+  return /^[a-zA-Z]+$/.test(string);
+};
+
+const VALIDINPUT = event => {
+  event.classList.remove("is-invalid");
+  event.classList.add("is-valid");
+};
+const INVALIDINPUT = event => {
+  event.classList.remove("is-valid");
+  event.classList.add("is-invalid");
+};
+
+const CARDVALIDATION = () => {
+  CARD.addEventListener("focusout", event => {
+    CHECKNUMBERS(CARD.value) &&
+    CARD.value.length >= 10 &&
+    CARD.value.length <= 15 &&
+    CARDVALIDATION(CARD.value)
+      ? VALIDINPUT(CARD)
+      : INVALIDINPUT(CARD);
+  });
+};
+const CVCVALIDATION = () => {
+  CVC.addEventListener("focusout", event => {
+    CHECKNUMBERS(CVC.value) && (CVC.value.length == 3 || CVC.value.length == 4)
+      ? VALIDINPUT(CVC)
+      : INVALIDINPUT(CVC);
+  });
+};
+
+const AMOUNTVALIDATION = () => {
+  AMOUNT.addEventListener("focusout", event => {
+    Number(AMOUNT.value) && AMOUNT.value > 0
+      ? VALIDINPUT(AMOUNT)
+      : INVALIDINPUT(AMOUNT);
+  });
+};
+
+const NAMEVALIDATION = () => {
+  NAME.addEventListener("focusout", event => {
+    CHECKSTRING(NAME.value) ? VALIDINPUT(NAME) : INVALIDINPUT(NAME);
+  });
+};
+
+const LASTNAMEVALIDATION = () => {
+  SURNAME.addEventListener("focusout", event => {
+    CHECKSTRING(SURNAME.value) ? VALIDINPUT(SURNAME) : INVALIDINPUT(SURNAME);
+  });
+};
+const CITYVALIDATION = () => {};
+CITY.addEventListener("focusout", event => {
+  CHECKSTRING(CITY.value) ? VALIDINPUT(CITY) : INVALIDINPUT(CITY);
+});
